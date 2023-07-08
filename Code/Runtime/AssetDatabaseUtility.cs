0ce81bee4c0434ef90bf882a7d6e00be2c97ddfc
HoSHIZA <needahentai@gmail.com>
2023-07-08 17:48:48 +0300

    Add AssetDatabaseUtility

---
+++ Code/Runtime/AssetDatabaseUtility.cs
@@ -0,0 +1,51 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+using Object = UnityEngine.Object;
+
+namespace ShizoGames.ShizoUtility
+{
+    /// <summary>
+    /// A utility class for working with assets in Unity.
+    /// </summary>
+    /// <remarks>
+    /// Only works in unity editor.
+    /// </remarks>
+    public static class AssetDatabaseUtility
+    {
+        /// <summary>
+        /// Loads all assets with type T using AssetDatabase.
+        /// </summary>
+        /// <typeparam name="T">Type of assets to be loaded.</typeparam>
+        /// <param name="type">Type of assets to be loaded.</param>
+        /// <returns>An array of loaded assets.</returns>
+        public static T[] LoadAllAssetsWithType<T>(Type type) where T : Object
+        {
+            var list = new List<T>();
+            
+            var guids = AssetDatabase.FindAssets($"t:{type}");
+            
+            foreach (var guid in guids)
+            {
+                T instance;
+                
+                try
+                {
+                    instance = AssetDatabase.LoadAssetAtPath<T>(AssetDatabase.GUIDToAssetPath(guid));
+                }
+                catch
+                {
+                    continue;
+                }
+                
+                if (instance == null) continue;
+                
+                list.Add(instance);
+            }
+            
+            return list.ToArray();
+        }
+    }
+}
+#endif
